d972059b32a7fc7d403fb41b0f252505b72d0ece
Xiaolong Gao <xiaolonggao521@gmail.com>
2024-10-18 21:39:39 -0700

    Draw triangle

--- random-engine/src/main/main.cpp
+++ random-engine/src/main/main.cpp
@@ -1,25 +1,136 @@
-#define GLFW_INCLUDE_VULKAN
+#include <GL/glew.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-int main() {
-  glfwInit();
+#include <stdexcept>
+#include <string>
+ 
+#include <stdlib.h>
+#include <stdio.h>
+ 
+static const char* vertex_shader_text = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+ 
+static const char* fragment_shader_text = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Hello, GLFW!", NULL, NULL);
-  if (!window) {
-      glfwTerminate();
-      return -1;
-  }
+static const float vertices[9] =
+{
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
+};
+ 
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+ 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+ 
+int main(void)
+{
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_array, vertex_shader, fragment_shader, program;
+ 
+    glfwSetErrorCallback(error_callback);
+ 
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+ 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+ 
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+ 
+    // init
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
 
-  glfwMakeContextCurrent(window);
+    GLenum error = glewInit();
+    if (GLEW_OK != error) {
+        glfwDestroyWindow(window);
+        throw std::runtime_error(
+            std::string("Error initializing glew: ") +
+            reinterpret_cast<const char *>(glewGetErrorString(error)));
+    }
 
-  while (!glfwWindowShouldClose(window)) {
-      // Rendering code here
+    glfwSwapInterval(1);
 
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-  }
+    // init shader program
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+ 
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+ 
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
 
-  glfwTerminate();
-  
-  return 0;
+    glLinkProgram(program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+ 
+    // generate objects
+    glGenVertexArrays(1, &vertex_array);
+    glGenBuffers(1, &vertex_buffer);
+
+    // bind
+    glBindVertexArray(vertex_array);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+ 
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(program);
+        glBindVertexArray(vertex_array);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+ 
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+ 
+    glfwDestroyWindow(window);
+ 
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
